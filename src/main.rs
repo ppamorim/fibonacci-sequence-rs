23a5719272cbded03dd972e96b90bc2da38a44cc
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2021-11-20 01:05:28 +0000

    Shame

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::time::Duration;
 use std::mem::replace;
 use std::{ thread, sync::{ Arc, Mutex }};
 
+use std::io::{self, BufRead};
+use std::sync::mpsc::{self, TryRecvError};
+
 extern crate num_cpus;
 
 fn main() {
 
-  let cores = num_cpus::get();
+  let cores = num_cpus::get() * 100;
 
   println!("Starting process using {} cores", cores);
   thread::sleep(Duration::from_secs(2));
@@ -23,6 +26,7 @@ fn main() {
   for i in 0..cores {
     let first_term: Arc<Mutex<BigUint>> = Arc::clone(&first_term);
     let last_term: Arc<Mutex<BigUint>> = Arc::clone(&last_term);
+    let mut count = 0;
     let handle: thread::JoinHandle<()> = thread::spawn(move || {
       loop {
         let temp: BigUint = last_term.lock().unwrap().clone();
@@ -34,11 +38,18 @@ fn main() {
 
         let mut last_term_mut = last_term.lock().unwrap();
         replace(&mut *last_term_mut, sum);
-        println!("\n\n{}", last_term_mut);
+        // println!("\n\n{}", last_term_mut);
+
+        count += 1;
+        if count % 1000 == 0 {
+          count = 0;
+          println!("{}\n", last_term_mut);
+        }
       }
     });
     handles.push(handle);
   }
+
   for thread in handles.into_iter() {
     thread.join().unwrap();
   }
