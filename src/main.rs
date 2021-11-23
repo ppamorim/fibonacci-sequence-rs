d827edb79304f20f3c8982ae3b0644f019875560
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2021-11-23 11:35:01 +0000

    Matrix multiplication

--- src/main.rs
+++ src/main.rs
@@ -21,28 +21,89 @@ extern crate num_cpus;
 // for _ in range(n):
 //     next(f)
 
+///
+/// Source from:
+/// https://vladris.com/blog/2018/02/11/fibonacci.html
+/// 
+/// 
+fn mul2x2(a: &[[BigUint; 2]; 2], b: &[[BigUint; 2]; 2]) -> [[BigUint; 2]; 2] {
+  [
+      [&a[0][0] * &b[0][0] + &a[1][0] * &b[0][1], &a[0][0] * &b[1][0] + &a[1][0] * &b[1][1]],
+      [&a[0][1] * &b[0][0] + &a[1][1] * &b[0][1], &a[0][1] * &b[1][0] + &a[1][1] * &b[1][1]],
+  ]
+}
+
+fn fast_exp2x2(a: [[BigUint; 2]; 2], n: i32) -> [[BigUint; 2]; 2] {
+  op_n_times(a, &mul2x2, n)
+}
+
+fn op_n_times<T, Op>(a: T, op: &Op, n: i32) -> T
+    where Op: Fn(&T, &T) -> T {
+    if n == 1 { return a; }
+
+    let mut result = op_n_times::<T, Op>(op(&a, &a), &op, n >> 1);
+    if n & 1 == 1 {
+        result = op(&a, &result);
+    }
+
+    result
+}
+
+fn fib4(n: i32) -> BigUint {
+  if n == 0 { return Zero::zero(); }
+  if n == 1 { return One::one(); }
+
+  let a = [
+      [One::one(), One::one()],
+      [One::one(), Zero::zero()],
+  ];
+
+  fast_exp2x2(a, n - 1)[0][0].clone()
+}
+
 fn main() {
 
   let n = 3000000;
-  let mut a: BigUint = Zero::zero();
-  let mut b: BigUint = One::one();
-  let mut c: BigUint = Zero::zero();
 
   let start = Instant::now();
 
-  for _ in 0..n {
-    c = &a + &b;
-    a = b;
-    b = c;
-  }
+  // for _ in 0..n {
+  //     let c = a + &b;
+  //     // This is a low cost way of swapping f0 with f1 and f1 with f2.
+  //     a = replace(&mut b, c);
+  // }
+  let result1 = fib4(n/3);
+  let result2 = fib4(n/2);
+  let result3 = fib4(n);
 
   let duration = start.elapsed();
-
-  print!("a: {}, b: {}", a, b);
 
   println!("Time elapsed: {:?}", duration);
 
 }
+
+// fn main() {
+
+//   let n = 3000000;
+//   let mut a: BigUint = Zero::zero();
+//   let mut b: BigUint = One::one();
+//   let mut c: BigUint;
+
+//   let start = Instant::now();
+
+//   for _ in 0..n {
+//       let c = a + &b;
+//       // This is a low cost way of swapping f0 with f1 and f1 with f2.
+//       a = replace(&mut b, c);
+//   }
+
+//   let duration = start.elapsed();
+
+//   print!("a: {}, b: {}", a, b);
+
+//   println!("Time elapsed: {:?}", duration);
+
+// }
 
 // fn main() {
 
